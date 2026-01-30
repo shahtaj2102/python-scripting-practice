d7f6538a3eb5a9bd5dcdbf1d318c90be1d187aed
Shahtaj singh Gill <157941958+shahtaj2102@users.noreply.github.com>
2026-01-30 18:31:52 -0500

    Improve input prompts in tip calculator
    Updated input prompts for better readability.

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,7 +1,8 @@
 
 print("Welcome to the tip calculator!")
-bill = float(input("What was the total bill? $"))
-tip = int(input("What percentage tip would you like to give? 10 12 15 "))
-people = int(input("How many people to split the bill? "))
+bill = float(input("What was the total bill?\n $"))
+tip = int(input("What percentage tip would you like to give?\n 10 12 15 /n"))
+people = int(input("How many people to split the bill?\n "))
 amount = ((bill / 100 * tip) + bill) / people
 print(f"The amount to be paid by each person is ${round(amount, 2)}")
+
