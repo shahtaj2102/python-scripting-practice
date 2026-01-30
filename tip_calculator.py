dde87f1749babd8844149e148c179a7a19ceb835
Shahtaj singh Gill <157941958+shahtaj2102@users.noreply.github.com>
2026-01-30 18:33:07 -0500

    Fix formatting issues in tip calculator

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,8 +1,9 @@
 
 print("Welcome to the tip calculator!")
-bill = float(input("What was the total bill?\n $"))
-tip = int(input("What percentage tip would you like to give?\n 10 12 15 /n"))
+bill = float(input("What was the total bill?\n$"))
+tip = int(input("What percentage tip would you like to give?\n 10 12 15 \n"))
 people = int(input("How many people to split the bill?\n "))
 amount = ((bill / 100 * tip) + bill) / people
 print(f"The amount to be paid by each person is ${round(amount, 2)}")
+
 
