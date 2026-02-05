bde276a414d2e83fd71c42b63f02d9aacc1373d6
Shahtaj singh Gill <157941958+shahtaj2102@users.noreply.github.com>
2026-02-04 23:58:23 -0500

    Enhance output message for heads or tails result

--- Heads_or_tails.py
+++ Heads_or_tails.py
@@ -2,6 +2,7 @@ import random
 
 random_heads_or_tails = random.randint(0, 1)
 if random_heads_or_tails == 0:
-    print("Heads")
+    print("Your outcome is:\n'Heads'")
 else:
-    print("Tails")
+    print("Your outcome is:\n'Tails'")
+
